032bb4f373fd6cdf30920888cdc39329544e6d5c
Roland Gustaw <Roland Gustaw>
2016-03-06 13:40:15 +0100

    Dodano komentarze

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
  *  Created on: 4 mar 2016
  *      Author: Roland
  */
-#include <avr/io.h>
+#include <avr/io.h> //biblioteka do sterowania wejœciem/wyjœciem
 #include <util/delay.h>
 
-int main(void){
+int main(void){ //void czyli pobiera nic, np. podnieœ, po³ó¿.
 
-	DDRB |= (1<<PB1);
+	DDRB |= (1<<PB1); //ustalamy czy dany port jest wejsciem, czy wyjsciem
 
 	while(1){
-		PORTB ^= (1<<PB1);
+		PORTB ^= (1<<PB1);// przypisujemy stan wysoki albo niski na konkretny port
 		_delay_ms (1000);
 	}
 }
