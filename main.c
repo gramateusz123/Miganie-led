f26bcbc0bd82a024a789ae2047f1c634ebc7c3a8
Roland Gustaw <Roland Gustaw>
2016-03-26 18:38:03 +0100

    ledy lewo-prawo, prawo-lewo

--- main.c
+++ main.c
@@ -13,15 +13,95 @@ int main(void){ //void czyli pobiera nic, np. podnieœ, po³ó¿.
 	DDRC = 0b11111111; //ustalamy czy dany port jest wejsciem, czy wyjsciem
 	PORTC = 0b00000001;
 	int licznik = 0;
+	int x = 0;
 	while(1){
-		if(licznik == 6)
-		{
-			licznik = 0;
-			PORTC = 0b00000001;
-		}
-		_delay_ms(150);
-		PORTC = (PORTC << 1);
-		licznik++;
+
+		if(x == 0){
+			if(licznik == 6 )
+			{
+				PORTC = 0b00100000;
+				licznik = 0;
+				x = 1;
+
+			}
+			else
+			{
+
+				_delay_ms (1000);
+				PORTC = (PORTC << 1);
+				licznik++;
+			}
+
+			}
+		else if(x == 1){
+
+				if(licznik == 6 )
+				{
+
+					PORTC = 0b00000001;
+					licznik = 0;
+					x = 0;
+
+				}
+				else
+				{
+					_delay_ms (1000);
+					PORTC = (PORTC >> 1);
+					licznik++;
+				}
+			}
 	}
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//		if (x == 0){//diody w lewo
+//			if(licznik == 6){
+//				licznik = 0;
+//				PORTC = 0b00100000;
+//				x = 1;
+//
+//			}
+//			else{
+//				_delay_ms(500);
+//				PORTC = (PORTC << 1);
+//				licznik++;
+//			}
+//
+//		}
+//		else if(x == 1){//diody w prawo
+//			if(licznik == 6){
+//				licznik = 0;
+//				PORTC = 0b00000001;
+//				x = 0;
+//
+//			}
+//			else{
+//				_delay_ms(500);
+//				PORTC = (PORTC >> 1);
+//				licznik++;
+//				}
+//			}
+//
