5d1414e528539e8441468021e9b64ea3e6a77193
Roland Gustaw <Roland Gustaw>
2016-03-26 15:57:09 +0100

    moja kombinacja led

--- main.c
+++ main.c
@@ -10,10 +10,24 @@
 
 int main(void){ //void czyli pobiera nic, np. podnieœ, po³ó¿.
 
-	DDRB |= (1<<PB1); //ustalamy czy dany port jest wejsciem, czy wyjsciem
+	DDRC = 0b11111111; //ustalamy czy dany port jest wejsciem, czy wyjsciem
 
 	while(1){
-		PORTB ^= (1<<PB1);// przypisujemy stan wysoki albo niski na konkretny port
+		PORTC = 0b00010101;// przypisujemy stan wysoki albo niski na konkretny port
+		_delay_ms (1000);
+		PORTC = 0b00101010;
+		_delay_ms (1000);
+		PORTC = 0b00111110;
+		_delay_ms (1000);
+		PORTC = 0b00111101;
+		_delay_ms (1000);
+		PORTC = 0b00111011;
+		_delay_ms (1000);
+		PORTC = 0b00110111;
+		_delay_ms (1000);
+		PORTC = 0b00101111;
+		_delay_ms (1000);
+		PORTC = 0b00011111;
 		_delay_ms (1000);
 	}
 }
